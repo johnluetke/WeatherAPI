9fadb840ddd83687a9169ea5abc9997c81cf362d
jluetke <john@johnluetke.net>
2012-05-02 18:41:57 -0700

    Adding new provider class for Google's unofficial weather service

--- Providers/iGoogle/iGoogleProvider.cs
+++ Providers/iGoogle/iGoogleProvider.cs
@@ -1,23 +1,29 @@
 using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Xml.XPath;
 
 using WeatherAPI;
 using WeatherAPI.Providers;
 
-namespace WearherAPI.Providers.iGoogle {
+namespace WeatherAPI.Providers.iGoogle {
 	
 	internal class iGoogleProvider : WeatherProvider, IWeather {
 		
 		private const string IG_API_URL = "http://www.google.com/ig/api?weather={0}";
-		private const string IG_XPATH_HEADER = "//data/current_condition/{0}";
+		private const string IG_XPATH_HEADER = "//xml_api_reply/weather/current_conditions/{0}";
 		
 		private XPathDocument _xpath;
 		private string _wwo_api_key;
+		
+		private Direction _windDirection;
 		
 		public iGoogleProvider () {
 
 		}
 		
-		public bool isAvailable() {
+		public override bool IsAvailable(){
 			return true;
 		}
 		
@@ -26,7 +32,7 @@ namespace WearherAPI.Providers.iGoogle {
 		}
 		
 		public override void Update () {
-			string url = String.Format(WWO_API_URL, "98121", WWO_API_FORMAT, _wwo_api_key);
+			string url = String.Format(IG_API_URL, Location);
 			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
 			
 			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
@@ -34,6 +40,84 @@ namespace WearherAPI.Providers.iGoogle {
 			
 			_xpath = new XPathDocument(new StringReader(xml));
 		}
+		
+		public double DegreesCelcius {
+			get {
+				string xpath = String.Format(IG_XPATH_HEADER, "temp_c/@data");
+				object val = _xpath.CreateNavigator().SelectSingleNode(xpath);
+				
+				return Double.Parse(val.ToString());
+			}
+		}
+
+		public double DegressFahrienhiet {
+			get {
+				string xpath = String.Format(IG_XPATH_HEADER, "temp_f/@data");
+				object val = _xpath.CreateNavigator().SelectSingleNode(xpath);
+				
+				return Double.Parse(val.ToString());
+			}
+		}
+
+		public double WindSpeedMPH {
+			get {
+				string xpath = String.Format(IG_XPATH_HEADER, "wind_condition/@data");
+				object val = _xpath.CreateNavigator().SelectSingleNode(xpath);
+				
+				Match m = Regex.Match(val.ToString(), "Wind: ([NESW]{1,2}) at ([0-9]{1,}) mph");
+				
+				_windDirection = (Direction)Enum.Parse(typeof(Direction), m.Groups[1].Value);
+				
+				return Double.Parse(m.Groups[2].Value);
+			}
+		}
+		
+		public double WindSpeedKPH {
+			get {
+				return WindSpeedMPH * 1.609344;
+			}
+		}
+		
+		public Direction WindDirection {
+			get {
+				return _windDirection;
+			}
+		}
+
+		public double CloudCover {
+			get {
+				return Double.NaN;
+			}
+		}
+		
+		public double Percipitation {
+			get {
+				return Double.NaN;
+			}
+		}
+
+		public double Humidity { 
+			get {
+				string xpath = String.Format(IG_XPATH_HEADER, "humidity/@data");
+				object val = _xpath.CreateNavigator().SelectSingleNode(xpath);
+				val = Regex.Replace(val.ToString(), "[^0-9]", "");
+				
+				return Double.Parse(val.ToString());
+			}
+		}
+		
+		public WeatherCondition Conditions {
+			get { 
+				string xpath = String.Format(IG_XPATH_HEADER, "condition/@data");
+				string val = _xpath.CreateNavigator().SelectSingleNode(xpath).ToString();
+				
+				switch (val) {
+					case "Partly Sunny": return WeatherCondition.PartlyCloudy;
+					default:
+						return (WeatherCondition)Enum.Parse(typeof(WeatherCondition), val);
+				}	
+			}
+		}	
 	}
 }
 
