d3525061a7ad20cd529e367b13c8ea7733f35146
John Luetke <john@johnluetke.net>
2013-03-24 16:14:04 -0700

    Adding aliases to Direction

--- src/main/java/WeatherAPI/Direction.java
+++ src/main/java/WeatherAPI/Direction.java
@@ -18,14 +18,64 @@ package WeatherAPI;
  * Represents the compass directions. Provides the 16 points of direction
  */
 public enum Direction {
-	N, E, S, W,	
-	NE, SE, NW, SW,
-	NNE, ENE, ESE, SSE,
-	NNW, WNW, WSW, SSW,
+	N("N", "North"),
+	E("E", "East"),
+	S("S", "South"),
+	W("W", "West"),	
 	
-	//North = N, East = E, South = S, West = W,
-	//Northeast = NE, Southeast = SE, Northwest = NW, Southwest = SW,
-	//NorthNortheast = NNE, EastNortheast = ENE, EastSoutheast = ESE, SouthSoutheast = SSE,
-	//NorthNorthwest = NNW, WestNorthwest = WNW, Westsouthwest = WSW, SouthSouthwest = SSW
+	NE("NE", "Northeast"),
+	SE("SE", "Southeast"),
+	NW("NW", "Northwest"),
+	SW("SW", "Southwest"),
 	
+	NNE("NNE", "North-Northeast"),
+	ENE("ENE", "East-Northeast"),
+	ESE("ESE", "East-southeast"),
+	SSE("SSE", "South-Southeast"),
+	
+	NNW("NNW", "North-Northwest"),
+	WNW("WNW", "West-Northwest"),
+	WSW("WSW", "West-Southwest"),
+	SSW("SSW", "South-Southwest");
+	
+	private String[] _aliases;
+	
+	/**
+	 * Creates a new direction with the given aliases
+	 * 
+	 * @param alias 
+	 */
+	private Direction(String... alias) {
+		_aliases = alias;
+	}
+	
+	/**
+	 * Gets all aliases for this Direction
+	 * 
+	 * @return 
+	 */
+	public String[] getAliases() {
+		return _aliases;
+	}
+
+	/**
+	 * More verbose way to convert a string into a Direction. Consider's Direction aliases
+	 * 
+	 * @param value the string to parse
+	 * 
+	 * @return Direction corresponding to the given string
+	 * 
+	 * @throws IllegalArgumentException if no match is found
+	 */
+	public static Direction getValue(String value) {
+		for (Direction d : Direction.values()) {
+			for (String alias : d.getAliases()) {
+				if (alias.equalsIgnoreCase(value)) {
+					return d;
+				}
+			}
+		}
+		
+		throw new IllegalArgumentException();
+	}	
 }
