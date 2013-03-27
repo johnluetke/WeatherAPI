bc593c369548baea38b49c9fa370fdc8d595cd3b
John Luetke <john@johnluetke.net>
2013-03-26 23:05:01 -0700

    Adding Zip Code tests

--- src/test/java/WeatherAPI/Tests/WeatherTest.java
+++ src/test/java/WeatherAPI/Tests/WeatherTest.java
@@ -51,7 +51,77 @@ public class WeatherTest {
 		}
 	}
 	
+	@Test
+	public void testZIPCodes() {
+		
+		int[] zipCodes = new int[] {
+			2045,
+			4062,
+			8528,
+			12528,
+			12941,
+			15061,
+			17344,
+			20057,
+			20078,
+			24221,
+			27323,
+			28450,
+			28480,
+			30114,
+			30645,
+			35961,
+			36033,
+			40295,
+			41554,
+			42753,
+			48105,
+			49653,
+			50328,
+			53545,
+			55345,
+			55718,
+			56473,
+			56552,
+			57326,
+			59114,
+			60189,
+			61025,
+			68381,
+			69022,
+			70763,
+			71066,
+			71419,
+			73491,
+			76903,
+			79783,
+			84716,
+			88042,
+			88119,
+			91012,
+			91978,
+			92173,
+			93675,
+			94284,
+			95209,
+			97436,
+		};
+		
+		for (int i = 0; i < zipCodes.length; i++) {
+			if (i != 0 && i % 9 == 0) {
+				try {
+					Thread.sleep(60 * 1000);
+				}
+				catch (Exception e) {
+					
+				}
+			}
+			printWeather(WeatherAPI.getWeather(zipCodes[i]));
+		}
+	}
+	
 	private void printWeather(IWeather w) {
+		System.out.println(String.format("--- %s ---", w.getLocationName()));
 		System.out.println(String.format("Degrees C:          %s", w.getDegreesCelcius()));
 		System.out.println(String.format("Degrees F:          %s", w.getDegressFahrienhiet()));
 		System.out.println(String.format("Wind (MPH):         %s", w.getWindSpeedMPH()));
